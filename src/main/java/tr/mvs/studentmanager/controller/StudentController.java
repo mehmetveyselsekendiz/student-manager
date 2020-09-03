270a720a58249a57b283f4b1d89aaeabfa6a09e1
Mehmet Veysel Sekendiz <mehmetveyselsekendiz@gmail.com>
2020-09-04 00:21:43 +0300

    get, create and delete student endpoints are added.

--- src/main/java/tr/mvs/studentmanager/controller/StudentController.java
+++ src/main/java/tr/mvs/studentmanager/controller/StudentController.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tr.mvs.studentmanager.model.Student;
 import tr.mvs.studentmanager.repository.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -18,12 +18,67 @@ public class StudentController {
     private StudentRepository studentRepository;
 
     @GetMapping(value = "/students")
-    public ResponseEntity<?> getAllStudents() {
+    public ResponseEntity<List<Student>> getAllStudents() {
         List<Student> students = this.studentRepository.findAll();
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(students);
+    }
+
+    @GetMapping(value = "/students/{id}")
+    public ResponseEntity<Student> getStudentById(
+            @PathVariable(name = "id", required = true) Long id) {
+        Optional<Student> optionalStudent = this.studentRepository.findById(id);
+
+        if (optionalStudent.isPresent()) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(optionalStudent.get());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @DeleteMapping(value = "/students")
+    public ResponseEntity<Student> deleteAllStudents() {
+        this.studentRepository.deleteAll();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
+    }
+
+    @DeleteMapping(value = "/students/{id}")
+    public ResponseEntity<Student> deleteStudentById(
+            @PathVariable(name = "id", required = true) Long id) {
+        Optional<Student> optionalStudent = this.studentRepository.findById(id);
+
+        if (optionalStudent.isPresent()) {
+            this.studentRepository.deleteById(id);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(optionalStudent.get());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @PostMapping(value = "/students")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student savedStudent = this.studentRepository.save(student);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(savedStudent);
     }
 }
