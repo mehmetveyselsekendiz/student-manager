6c2fdfbd3ec216934b536b7a6492b48774354b83
Mehmet Veysel Sekendiz <mehmetveyselsekendiz@gmail.com>
2020-09-08 20:58:42 +0300

    Lecture and lecturer repositories, models and controllers are created.

--- src/test/java/tr/mvs/studentmanager/StudentManagerApplicationTests.java
+++ src/test/java/tr/mvs/studentmanager/StudentManagerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
